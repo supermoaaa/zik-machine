0498d607d971b9641d52daa58d6ef87739f5f811
supermoaaa <laurent.castanier100@gmail.com>
2020-09-30 10:55:34 +0200

    centred button on midi conf panel

--- pad_conf_midi.gd
+++ pad_conf_midi.gd
@@ -9,14 +9,14 @@ func _ready():
 	var configFile = ConfigFile.new()
 	configFile.load(conf_file)
 	conf_machine = configFile.get_value("MACHINE", "active_machine")
-	$GridContainer.columns = pad/4
+	$CenterMidi/GridContainer.columns = pad/4
 	for bt in pad:
 		var testbt = Button.new()
 		#testbt.margin_top = 30
 		#testbt.margin_bottom = 30
 		testbt.text = "test"+str(bt)
 		testbt.name = "pad"+str(bt)
-		$GridContainer.add_child(testbt)
+		$CenterMidi/GridContainer.add_child(testbt)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
