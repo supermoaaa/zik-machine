9b3c8c1b016f559dbda7477e69188ef1bde8d63c
supermoaaa <laurent.castanier100@gmail.com>
2020-10-30 14:17:16 +0100

    regenerate record folder file after recording

--- Rec_Button.gd
+++ Rec_Button.gd
@@ -31,7 +31,34 @@ func _on_Rec_Button_toggled(button_pressed):
 		recordingeffectmaster.set_recording_active(false)
 		var final_recording = recordingeffectmaster.get_recording()
 		final_recording.save_to_wav(save_song+song_name)
+		
+		var list_rec = []
 
+		var file = File.new()		
+		if not file.file_exists("res://record_path.sav"):
+			print("No file detected")
+			return
+		if file.open("res://record_path.sav", File.READ) != 0:
+			print("Error opening file")
+			return
+		
+		while not file.eof_reached():
+			list_rec.append(file.get_line())
+		list_rec.append(str(save_song+song_name))
+		file.close()
+		
+
+		if file.open("res://record_path.sav", File.WRITE) != 0:
+			print("Error opening file")
+			return
+
+		for rec_file in list_rec:
+			file.store_line(rec_file)
+		file.close()
+		#var rep = get_tree().get_root().get_node("/body/File_rep")
+		#var app_rec = rep.get_children("record").create_item("record")
+		#app_rec.set_text(0, song_name)
+		
 
 
 func _on_LineEdit_text_entered(new_text):
