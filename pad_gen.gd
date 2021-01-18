544f5d7e83ecd80d34d1d1c9bbbd217a550344d4
supermoaaa <laurent.castanier100@gmail.com>
2021-01-18 06:45:34 +0100

    better list for active stream

--- pad_gen.gd
+++ pad_gen.gd
@@ -83,7 +83,7 @@ func Global_sync():
 	time = OS.get_ticks_msec()
 	if time - last_ticks >= master_Tempo:
 		for sample in snyc_list:
-			var active_stream = get_node(str(sample))
+			var active_stream = get_node("play_instance" + str(sample))
 			active_stream.play()
 		last_ticks = time
 	
@@ -465,8 +465,8 @@ func _on_SpinBox_value_changed(value):
 	
 func _active_snyc_bpm(toggled, pad_name):
 	if toggled == true:
-		snyc_list.append("play_instance" + str(pad_name))
+		snyc_list.append(pad_name)
 	else:
-		var idx = snyc_list.find("play_instance" + str(pad_name))
+		var idx = snyc_list.find(pad_name)
 		snyc_list.remove(idx)
 
